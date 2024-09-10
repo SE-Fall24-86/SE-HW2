d521bac354f85f97e0afd929e1f2e1879e0cfb9d
Rohit Sriram <58294503+rohit-sram@users.noreply.github.com>
2024-09-10 23:30:28 +0000

    Merge sort code fixed in hw_fixed.py

--- hw2 references/hw2_fixed.py
+++ hw2 references/hw2_fixed.py
@@ -10,30 +10,6 @@ def mergeSort(arr):
     return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
 
 
-# def recombine_fixed(leftArr, rightArr):
-#     leftIndex = 0
-#     rightIndex = 0
-#     mergeArr = [None] * (len(leftArr) + len(rightArr))
-
-#     while leftIndex < len(leftArr) and rightIndex < len(rightArr):
-#         if leftArr[leftIndex] < rightArr[rightIndex]:
-#             mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
-#             leftIndex += 1
-#         else:
-#             mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
-#             rightIndex += 1
-
-#     while leftIndex < len(leftArr):
-#         mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
-#         leftIndex += 1
-
-#     while rightIndex < len(rightArr):
-#         mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
-#         rightIndex += 1
-
-#     return mergeArr
-
-
 def recombine(leftArr, rightArr):
     leftIndex = 0
     rightIndex = 0
