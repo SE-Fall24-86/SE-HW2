0b8a651a74f84fd984c47a8fbb8fbbfa5cf67873
Rohit Sriram <58294503+rohit-sram@users.noreply.github.com>
2024-09-10 23:29:18 +0000

    Autopep8 formatting done.

--- hw2 references/hw2_fixed.py
+++ hw2 references/hw2_fixed.py
@@ -10,7 +10,31 @@ def mergeSort(arr):
     return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
 
 
-def recombine_fixed(leftArr, rightArr):
+# def recombine_fixed(leftArr, rightArr):
+#     leftIndex = 0
+#     rightIndex = 0
+#     mergeArr = [None] * (len(leftArr) + len(rightArr))
+
+#     while leftIndex < len(leftArr) and rightIndex < len(rightArr):
+#         if leftArr[leftIndex] < rightArr[rightIndex]:
+#             mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
+#             leftIndex += 1
+#         else:
+#             mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
+#             rightIndex += 1
+
+#     while leftIndex < len(leftArr):
+#         mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
+#         leftIndex += 1
+
+#     while rightIndex < len(rightArr):
+#         mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
+#         rightIndex += 1
+
+#     return mergeArr
+
+
+def recombine(leftArr, rightArr):
     leftIndex = 0
     rightIndex = 0
     mergeArr = [None] * (len(leftArr) + len(rightArr))
@@ -34,28 +58,8 @@ def recombine_fixed(leftArr, rightArr):
     return mergeArr
 
 
-def recombine(leftArr, rightArr):
-    leftIndex = 0
-    rightIndex = 0
-    mergeArr = [None] * (len(leftArr) + len(rightArr))
-    while leftIndex < len(leftArr) and rightIndex < len(rightArr):
-        if leftArr[leftIndex] < rightArr[rightIndex]:
-            rightIndex += 1
-            mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
-        else:
-            leftIndex += 1
-            mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
-
-    for i in range(rightIndex, len(rightArr)):
-        mergeArr[leftIndex + rightIndex] = rightArr[i]
-
-    for i in range(leftIndex, len(leftArr)):
-        mergeArr[leftIndex + rightIndex] = leftArr[i]
-
-    return mergeArr
-
-
 arr = rand.random_array([None] * 20)
+# arr = [5, 3, 2, 1, 8, 10, 11, 9, 23]
 arr_out = mergeSort(arr)
 
 print(arr_out)
